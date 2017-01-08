a369a7abac38daaa8dd0f505d80f7314e42f8a6b
harshdoshi999 <harshdoshi999@gmail.com>
2017-01-08 17:43:15 +0530

    added form component's files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  template: `
+    <p>
+      form Works!
+    </p>
+  `,
+  styles: []
+})
+export class FormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
